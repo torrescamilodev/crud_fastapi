ad57f605c5d9860527aec31c7e2ca33bbbba4bb0
Camilo Torres <torrescamilodev@gmail.com>
2023-05-02 00:11:43 -0500

    Primer commit, se crea el entorno virtual que contiene FastApi y uvicorn, se crea un esquema general de la aplicacion

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI() # Estamos creando una instancia de FastApi
+app.title = "Mi apliación con FastAPI"
+app.version = "0.0.1"
+
+
+@app.get('/', tags=['home']) # Creamos el EndPoint
+def message():
+    return "Hello world!"
+
+# Para correr la aplicacion usamos el comando 'uvicorn main:app' y si queremos que se recargue automaticamente agregamos depues de app '--reload' y para asignarle un puerto '--port numero_del_puerto' y para que se pueda ver en toda la red '--host 0.0.0.0
